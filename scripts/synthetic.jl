a3e6aaea9cf851d990616804b6fead6a5ae28b01
Matteo Smerlak <matteo.smerlak@gmail.com>
2024-06-26 09:30:45 +0200

    nu = 4 in synthetic.jl

--- scripts/synthetic.jl
+++ scripts/synthetic.jl
@@ -12,19 +12,19 @@ using DataFrames
 σ = nanstd(ETF_returns.x)
 T = 252
 S = sqrt(T) * μ / σ
-ΔS = sqrt(1 + S^2/2)
-N = 100_000
+ΔS = sqrt(1 + S^2 / 2)
+N = 1_000_000
 Random.seed!(123)
 
 
 # Generate data
-synthetic = DataFrame(ν = Float64[], label=String[], m=Float64[], s=Float64[], sharpe=Float64[], sortino = Float64[])
-for ν in (3, Inf)
+synthetic = DataFrame(ν=Float64[], label=String[], m=Float64[], s=Float64[], sharpe=Float64[], sortino=Float64[])
+for ν in (4, Inf)
     dist = isfinite(ν) ? TDist(μ, σ, ν) : Normal(μ, σ)
     label = ν < Inf ? "Student(ν = $ν)" : "Normal"
     for _ in 1:N
         x = rand(dist, T)
-        push!(synthetic, 
-        [ν, label, mean(x), std(x), sqrt(T) * mean(x)/std(x), sqrt(T) * mean(x)/downside_std(x)])
+        push!(synthetic,
+            [ν, label, mean(x), std(x), sqrt(T) * mean(x) / std(x), sqrt(T) * mean(x) / downside_std(x)])
     end
 end
