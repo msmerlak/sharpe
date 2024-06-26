e0d2b23614d43abc2156eb98504fd43ee74d202c
Matteo Smerlak <matteo.smerlak@gmail.com>
2024-06-26 09:15:54 +0200

    vol-scaled returns, nu = 4

--- scripts/ETFs.jl
+++ scripts/ETFs.jl
@@ -8,25 +8,25 @@ using Dates
 using Statistics, StatsBase
 
 
-logreturns(prices) = [NaN, log.(prices[2:end]./prices[1:end-1])...]
+logreturns(prices) = [NaN, log.(prices[2:end] ./ prices[1:end-1])...]
 
 riskfree_data = CSV.read(datadir("RIFLGFCY01NA.csv"), DataFrame)
 
 riskfree = riskfree = Dict(
-    Year(row.DATE) => log(1 + row.RIFLGFCY01NA/100)
+    Year(row.DATE) => log(1 + row.RIFLGFCY01NA / 100)
     for row in eachrow(riskfree_data)
-        )
+)
 
 ETF_metadata = CSV.read(datadir("kaggle-funds/ETFs.csv"), DataFrame)
 ETF_data = CSV.read(datadir("kaggle-funds/ETF-prices.csv"), DataFrame)
 
 
-ETF_returns = @chain innerjoin(ETF_data, ETF_metadata, on = :fund_symbol) begin
+ETF_returns = @chain innerjoin(ETF_data, ETF_metadata, on=:fund_symbol) begin
     @transform(:year = Year.(:price_date))
-    @by([:fund_category, :fund_symbol, :year], 
-        :x = logreturns(:adj_close) .- log(1 + riskfree[first(:year)])/length(:year),
+    @by([:fund_category, :fund_symbol, :year],
+        :x = logreturns(:adj_close) .- log(1 + riskfree[first(:year)]) / length(:year),
         :T = length(:adj_close),
-        :year, 
+        :year,
         :fund_category
     )
     @subset(:T .== 252)
@@ -36,9 +36,9 @@ end
 
 ETF = @chain ETF_returns begin
     @by([:fund_category, :fund_symbol, :year],
-    :m = nanmean(:x), 
-    :s = nanstd(:x),
-    :sharpe = sqrt(length(:year)) .* nanmean(:x) ./ nanstd(:x),
-    :sortino = sqrt(length(:year)) .* nanmean(:x) ./ downside_std(:x)
+        :m = nanmean(:x),
+        :s = nanstd(:x),
+        :sharpe = sqrt(length(:year)) .* nanmean(:x) ./ nanstd(:x),
+        :sortino = sqrt(length(:year)) .* nanmean(:x) ./ downside_std(:x)
     )
 end
